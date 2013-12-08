c125e2b73b448f893d6b3f71d535fa3bc8d5b6bc
kordano <konrad.kuehne@rocketmail.com>
2013-12-08 11:39:18 +0100

    restructure websocket connection

--- src/cljs/geschichte/platform.cljs
+++ src/cljs/geschichte/platform.cljs
@@ -15,29 +15,26 @@
 
 ;; --- WEBSOCKET CONNECTION ---
 
-(def websocket* (atom nil))
+(defn put! [channel data]
+  (.send channel (str data)))
 
-(defn put! [data]
-  (.send @websocket* (str data)))
+(defn- take! [message]
+  (log (str "data received:" (read-string message))))
 
-(defn- take! [message fun]
-  (fun (str "data received:" (read-string message))))
+(defn client-connect! [address]
+  (let [channel (js/WebSocket. (str "ws://" address))]
+    (log (str "establishing websocket connection with " address))
+    (doall
+     (map #(aset channel (first %) (second %))
+          [["onopen" (fn [] (log "channel opened"))]
+           ["onclose" (fn [] (log "channel closed"))]
+           ["onerror" (fn [e] (log (str "ERROR:" e)))]
+           ["onmessage" (fn [m] (let [data (.-data m)] (take! data)))]]))
+    (log "websocket loaded.")
+    channel))
 
-(defn client-connect! [address fun]
-  (log (str "establishing websocket connection with " address))
-  (reset! websocket* (js/WebSocket. (str "ws://" address)))
-  (doall
-   (map #(aset @websocket* (first %) (second %))
-        [["onopen" (fn [] (do
-                           (log "channel opened")))]
-         ["onclose" (fn [] (log "channel closed"))]
-         ["onerror" (fn [e] (log (str "ERROR:" e)))]
-         ["onmessage" (fn [m] (let [data (.-data m)] (take! data log)))]]))
-  (log "websocket loaded."))
-
-
-#_(client-connect! "localhost:9090" log)
-#_(put! {:type :publish
+#_(def the-channel (client-connect! "localhost:9090"))
+#_(put! the-channel {:type :publish
          :user "user@mail.com"
          :repo #uuid"22aa0537-6e66-43e4-bda2-2b4211e0e4ec"
          :meta {:causal-order {#uuid "befcadd8-eb77-4565-b9fe-a77bac179aec" #{#uuid "b189b9f4-0901-4a39-a1c9-a0266254fbd3"}, #uuid "b189b9f4-0901-4a39-a1c9-a0266254fbd3" #{}, :root #uuid "b189b9f4-0901-4a39-a1c9-a0266254fbd3"}, :last-update #inst "2013-12-05T23:36:43.320-00:00", :head "master", :public true, :branches {"master" #{#uuid "befcadd8-eb77-4565-b9fe-a77bac179aec"}}, :schema {:version 1, :type "http://github.com/ghubber/geschichte"}, :pull-requests {}, :id #uuid "22aa0537-6e66-43e4-bda2-2b4211e0e4ec", :description "test repo"}})
