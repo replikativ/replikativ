3b6174c4964ae98253f2dd1f663c9be3896b9cd3
Konrad Kühne <konnykuehne@gmail.com>
2017-06-18 23:09:00 +0200

    further testing of js api

--- resources/public/browser_test.js
+++ resources/public/browser_test.js
@@ -1,14 +1,14 @@
 var r = replikativ.js;
 
 var user = "mail:alice@stechuhr.de";
-var ormapId = cljs.core.uuid("07f6aae2-2b46-4e44-bfd8-058d13977a8a");
+var ormapId = r.createUUID("07f6aae2-2b46-4e44-bfd8-058d13977a8a");
 var uri = "ws://127.0.0.1:31778";
 
 var props = {captures: []};
 
 var streamEvalFuncs = {"add": function(old, params) {
   var oldCaptures = old.captures;
-  var newCaptures = oldCaptures.push(params);
+  oldCaptures.push(params);
   return {captures: oldCaptures};
 }};
 
@@ -30,14 +30,14 @@ function setupReplikativ() {
     sync.stream = r.streamIntoIdentity(stage, user, ormapId, streamEvalFuncs, props)
     return r.createOrMap(stage, {id: ormapId, description: "captures"})
   }, logError).then(function() {
-    return r.connect(sync.stage, uri);
-  }, logError).then(function () {
+    //return r.connect(sync.stage, uri);
+  }, logError);/*.then(function () {
     console.log("stage connected!")
-  }, logError);
+  }, logError);*/
 }
 
 function checkIt(value) {
-  r.associate(sync.stage, user, ormapId, hasch.core.uuid(cljs.core.js__GT_clj(value)), [["add", value]])
+  r.associate(sync.stage, user, ormapId, r.hashIt(r.toEdn(value)), [["add", value]])
     .then(function(result) {
       console.log("associated with " + value);
     }, logError);
--- src/replikativ/js.cljs
+++ src/replikativ/js.cljs
@@ -3,6 +3,9 @@
   (:require [replikativ.peer :as peer]
             [replikativ.stage :as stage]
             [replikativ.crdt.ormap.stage :as ormap-stage]
+            [goog.net.WebSocket]
+            [goog.Uri]
+            [goog.events]
             [replikativ.crdt.ormap.realize :as ormap-realize]
             [konserve.memory :as mem]
             [kabel.client :refer [client-connect!]]
@@ -10,6 +13,12 @@
             [superv.async :refer [S]]
             [replikativ.crdt.ormap.core :as ormap])
   (:require-macros [superv.async :refer [go-loop-try go-try]]))
+
+(defn on-node? []
+  (and (exists? js/process)
+       (exists? js/process.versions)
+       (exists? js/process.versions.node)
+       true))
 
 (defn- promise [ch]
   (js/Promise.
@@ -65,10 +74,42 @@
                 (v old (clj->js params)))])
          (reduce (fn [m [k v]] (assoc m k v)) {}))))
 
+(defn eval-fns->component [eval-fns component]
+  (let [eval-fns (js->clj eval-fns)]
+    (->> (for [[k v] eval-fns]
+           [k (fn [old params]
+                ;; TODO: check params if binary
+                (v old (clj->js params)))])
+         (reduce (fn [m [k v]] (assoc m k v)) {}))))
 
 (defn ^:export streamIntoIdentity [stage user crdt-id stream-eval-fns target]
   (ormap-realize/stream-into-identity! stage [user crdt-id] (eval-fns->js stream-eval-fns) target))
 
+(defn ^:export streamIntoReactComponent [stage user crdt-id stream-eval-fns component]
+  (ormap-realize/stream-into-identity! stage [user crdt-id] (eval-fns->js stream-eval-fns) component))
+
+(defn ^:export createUUID [s]
+  (cljs.core/uuid s))
+
+(defn ^:export toEdn [o] (js->clj o))
+
+(defn ^:export hashIt [o] (hasch.core/uuid o))
+
+(when ^boolean js/COMPILED
+  (set! js/goog.global js/global))
+
+(set! (.-exports js/module) #js {:clientPeer         clientPeer
+                                 :connect            connect
+                                 :onNode             on-node?
+                                 :createStage        createStage
+                                 :newMemStore        newMemStore
+                                 :createOrMap        createOrMap
+                                 :associate          associate
+                                 :clientConnect      client-connect!
+                                 :createUUID         cljs.core/uuid
+                                 :toEdn              cljs.core/js->clj
+                                 :hashIt             hasch.core/uuid
+                                 :streamIntoIdentity streamIntoIdentity})
 
 (comment
   (defn on-node? []
@@ -82,10 +123,4 @@
     (set! js/goog.global js/global))
   (nodejs/enable-util-print!)
   (set! cljs.core/*main-cli-fn* -main)
-  (set! (.-exports js/module) #js {:clientPeer clientPeer
-                                   :connect connect
-                                   :createStage createStage
-                                   :newMemStore newMemStore
-                                   :createOrMap createOrMap
-                                   :associate associate
-                                   :streamIntoIdentity streamIntoIdentity}))
+  )
